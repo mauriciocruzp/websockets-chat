d8ece45178d1978dbdc3bbd431edce902988bec0
Mauricio Cruz <mawcp7@gmail.com>
2023-03-01 21:25:04 -0600

    reinit project

--- client/src/atoms/ExternalMessage/ExternalMessage.jsx
+++
@@ -1,9 +0,0 @@
-function ExternalMessage() {
-    return (
-        <div className="message w-100 d-flex justify-content-start">
-            <div className="w-25 bg-success rounded-3 px-3 d-flex align-items-center fw-bold">Hoal</div>
-        </div>
-    );
-}
-
-export default ExternalMessage;
