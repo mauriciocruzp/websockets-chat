241823d02f1ca592684c3af76c5a984741c1c9f6
Mauricio Cruz <mawcp7@gmail.com>
2023-03-01 18:37:58 -0600

    fix navigation bar

--- client/src/organisms/NavigationBar/NavigationBar.jsx
+++ client/src/organisms/NavigationBar/NavigationBar.jsx
@@ -1,6 +1,6 @@
 function NavigationBar() {
     return (
-        <nav class="navbar bg-dark">
+        <nav class="navbar bg-success position-fixed w-100">
             <div class="container-fluid">
                 <a class="navbar-brand text-light" href="#">
                     Chat Websockets
