1079a7ac42494ae2dabf13e25b25c9ef95979148
Mauricio Cruz <mawcp7@gmail.com>
2023-03-01 18:38:58 -0600

    call sections

---
+++ client/src/molecules/UsersList/UserList.jsx
@@ -0,0 +1,9 @@
+function UserList() {
+    return (
+        <div className="w-25">
+            j
+        </div>
+    );
+}
+
+export default UserList;
--- client/src/organisms/ChatSection/ChatSection.jsx
+++ client/src/organisms/ChatSection/ChatSection.jsx
@@ -1,11 +1,16 @@
+import InputMessage from "../../molecules/InputMessage/InputMessage";
+import MessagesScreen from "../../molecules/MessagesScreen/MessagesScreen";
+import UserList from "../../molecules/UsersList/UserList";
+
 function ChatSection() {
     return (
         <div className="h-100 d-flex flex-column">
-            <div className="h-75 bg-success">
-                w
+            <div className="h-75 shadow-sm pb-3 d-flex flex-row">
+                <MessagesScreen />
+                <UserList />
             </div>
-            <div className="h-25 bg-danger">
-                f
+            <div className="h-25 d-flex align-items-center">
+                <InputMessage />
             </div>
         </div>
     );
